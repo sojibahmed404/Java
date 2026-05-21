d74413feb09391612f24414579574d682c0df646
Md Sojib Ahmed <mdsojibahmed544@gmail.com>
2026-05-21 11:12:41 +0600

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many number  :");
+        int n = input.nextInt();
+        int first = 0;
+        int second = 1;
+        int fibo;
+        System.out.println(first + " " + second);
+        for (int i = 3; i <= n; i++) {
+            fibo = first + second;
+            System.out.print("" + fibo);
+            first = second;
+            second = fibo;
+
+        }
+    }
+}
